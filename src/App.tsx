4ce7462734dd2fa48be61d8ce15b607ac7cdfc85
marcobonera <marcobonera@MacBook-Pro-di-Marco.local>
2019-05-16 09:18:42 +0200

    add first app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import './App.css';
 
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+  text : string
+  complete : boolean
+}
+
+interface ITodo2 extends ITodo {
+  tags : string[]
+}
+
 export default function App(): JSX.Element {
+ const [value, setValue] = useState<string>('');
+ const [todos, setTodos] = useState<ITodo[]>([]);
+ 
+ const handleSubmit = (e:FormElem):void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+ }
+
+ const addTodo = (text:string):void =>{
+   const newTodos : ITodo[] = [...todos, {text, complete : false}]
+   setTodos(newTodos);
+ }
+
+ const completeTodo = (index: number):void =>{
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+ }
+
+ const removeTodo = (index:number) :void =>{
+  const newTodos: ITodo[] = [...todos];
+  newTodos.splice(index,1);
+  setTodos(newTodos);
+ }
 
   return (
-    <div className="App">
-    </div>
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" required value={value} onChange={e=>setValue(e.target.value)}></input>
+        <button type="submit">Add Todo</button>
+      </form>
+      <section>
+        {todos.map((todo :ITodo, index:number)=>{
+           return <Fragment key={index}>
+              <div style={{textDecoration: todo.complete ? 'line-through' : ''}}>{todo.text}</div>
+              <button type='button' onClick={()=> completeTodo(index)}>{todo.complete ? 'Incomplete' : 'Complete'}</button>
+              <button type='button' onClick ={()=> removeTodo(index)}>Remove</button>
+           </Fragment>          
+        })}
+      </section>
+    </Fragment>    
   );
 }
